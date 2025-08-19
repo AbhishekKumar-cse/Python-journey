15065d22534f2d126ada2acaf9bc1a21bb320018
Abhishek Kumar <47770860+AbhishekKumar-cse@users.noreply.github.com>
2025-08-19 20:49:33 +0000

    update guessing game

--- CarGame(notgraphical).py
+++ CarGame(notgraphical).py
@@ -1,10 +1,19 @@
 command = ""
-while command.lower() != "quit":
-    command = input(">")
-    if command.lower() == "start":
-        print("Car started...")
-    elif command.lower() == "stop":
-        print("Car stopped.")
+started = False
+while True:
+    command = input(">").lower()
+    if command == "start":
+        if started:
+            print("Car is already started...")
+        else:
+            started = True
+            print("Car start ...")
+    elif command == "stop":
+        if not started:   
+            print("Car is already stopped...")
+        else:
+            started = False
+            print("Car stopped...")
     elif command.lower() == "help":
         print("Available commands: start, stop, quit")
     elif command == "quit":
